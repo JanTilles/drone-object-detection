66e3dbf1bb5709baebfce78837c43a66230c6e68
Tilles Jan (Ext) <jan.tilles@lindstromgroup.com>
2025-03-11 09:43:12 +0200

    Add datafolders

--- create_datafolders.py
+++ create_datafolders.py
@@ -12,6 +12,15 @@ def create_data_folders(base_path):
         path = os.path.join(base_path, folder)
         os.makedirs(path, exist_ok=True)
         print(f'Created folder: {path}')
+        
+        # Create a .gitkeep file in each folder
+        gitkeep_path = os.path.join(path, '.gitkeep')
+        with open(gitkeep_path, 'w') as f:
+            pass
+        if os.path.exists(gitkeep_path):
+            print(f'Created .gitkeep in: {gitkeep_path}')
+        else:
+            print(f'Failed to create .gitkeep in: {gitkeep_path}')
 
 if __name__ == '__main__':
     base_path = 'c:/Users/extjtilles/Documents/Work/Python/drone-object-detection/dataset'
